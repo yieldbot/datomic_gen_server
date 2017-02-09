b5c9e6778854e6ed1fbffc6f402b110a1984c6d7
Geoffrey Clements <gclements@yieldbot.com>
2017-02-09 08:49:45 -0500

    Update datomic to 0.9.5554

--- priv/datomic_gen_server_peer/project.clj
+++ priv/datomic_gen_server_peer/project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [clojure-erlastic "0.3.1"]
-                 [com.datomic/datomic-pro "0.9.5350"]
+                 [com.datomic/datomic-pro "0.9.5554"]
                  [vvvvalvalval/datomock "0.1.0"]
                  [net.phobot.datomic/seed "3.0.0"]
                 ]
@@ -15,13 +15,13 @@
 
 ; If you are using Datomic Pro, the repository above will be necessary and you'll
 ; need your credentials for it. You'll need to install gpg for this.
-; 
+;
 ; First write your credentials map to ~/.lein/credentials.clj like so:
-; 
+;
 ;   {#"my\.datomic\.com" {:username "[USERNAME]"
 ;                         :password "[LICENSE_KEY]"}}
 ; Then encrypt it with gpg:
-; 
+;
 ;   $ gpg --default-recipient-self -e ~/.lein/credentials.clj > ~/.lein/credentials.clj.gpg
 ;
 ; Remember to delete the plaintext credentials.clj once you've encrypted it.
